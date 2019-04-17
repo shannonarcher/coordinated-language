90357c7b4f2a933334b7adc9055074fa153b9d02
Shannon Archer <shannon.archer0@gmail.com>
2019-04-17 09:09:23 -0500

    project names generated, last 10 names generated displayed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var generate = require('project-name-generator');
+var Cookies = require('cookies');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const project = generate();
+
+  const cookies = new Cookies(req, res);
+
+  let pastProjects;
+  try {
+    pastProjects = JSON.parse(cookies.get('past-projects'));
+  } catch {
+    pastProjects = [];
+  }
+
+  cookies.set('past-projects', JSON.stringify([
+    project.dashed,
+    ...pastProjects,
+  ].slice(0, 11)));
+
+  res.render('index', {
+    project,
+    pastProjects,
+  });
 });
 
 module.exports = router;
